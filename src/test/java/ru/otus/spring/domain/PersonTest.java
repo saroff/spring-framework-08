ff30ea65a06df325ed7ae6418e6b36036c982e60
ydvorzhetskiy <dvorzhetskij@mail.ru>
2018-07-20 22:48:19 +0600

    Final

---
+++ src/test/java/ru/otus/spring/domain/PersonTest.java
@@ -0,0 +1,28 @@
+package ru.otus.spring.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DataJpaTest
+public class PersonTest {
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    public void saveAndGet() {
+        Person person = new Person("Ivan");
+        Person fromDb = em.persistAndFlush(person);
+        assertThat(fromDb.getId()).isNotZero();
+        assertThat(fromDb.getName()).isEqualTo(person.getName());
+    }
+}
